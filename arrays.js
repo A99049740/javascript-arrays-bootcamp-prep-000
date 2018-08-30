942fe1ed67cf95c4ffe0442b26b5401130588747
jill zhang <talktojillzhang@gmail.com>
2018-08-30 17:57:04 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  newArray = [element,...array]
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element){
+  array.unshift(element)
+  return array
+
+}
+function addElementToBeginningOfArray(array, element){
+  newArray = [element,...array]
+  return newArray
+}
